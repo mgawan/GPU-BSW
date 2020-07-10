f4e3eff8f602aa8016b1cf5ea1acc3914c62a2ee
Richard Barnes <rbarnes@umn.edu>
2020-07-10 09:09:18 -0600

    Make some stuff in driver arguments const

--- driver.hpp
+++ driver.hpp
@@ -19,19 +19,32 @@ namespace gpu_bsw_driver{
 
 // for storing the alignment results
 struct alignment_results{
-  short* ref_begin;
-  short* query_begin;
-  short* ref_end;
-  short* query_end;
-  short* top_scores;
+  short* ref_begin = nullptr;
+  short* query_begin = nullptr;
+  short* ref_end = nullptr;
+  short* query_end = nullptr;
+  short* top_scores = nullptr;
 };
 
-void
-kernel_driver_dna(std::vector<std::string> reads, std::vector<std::string> contigs, alignment_results *alignments, short scores[4]);
 
 
-void
-kernel_driver_aa(std::vector<std::string> reads, std::vector<std::string> contigs, alignment_results *alignments, short scoring_matrix[], short openGap, short extendGap);
+void kernel_driver_dna(
+  const std::vector<std::string> &reads,
+  const std::vector<std::string> &contigs,
+  gpu_bsw_driver::alignment_results *const alignments,
+  const short scores[4]
+);
+
+
+
+void kernel_driver_aa(
+  const std::vector<std::string> &reads,
+  const std::vector<std::string> &contigs,
+  gpu_bsw_driver::alignment_results *const alignments,
+  const short scoring_matrix[],
+  const short openGap,
+  const short extendGap
+);
 
 void
 verificationTest(std::string rstFile, short* g_alAbeg, short* g_alBbeg, short* g_alAend,
