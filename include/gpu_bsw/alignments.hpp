fe1b0a8e2fdc59bcc6f294ebb206e718768f2f57
Richard Barnes <rbarnes@umn.edu>
2020-07-10 17:07:07 -0600

    Initialize pointers to nullptr

--- include/gpu_bsw/alignments.hpp
+++ include/gpu_bsw/alignments.hpp
@@ -2,14 +2,14 @@
 #define ALIGNMENTS_HPP
 
 class gpu_alignments{
-    public:
-    short* ref_start_gpu;
-    short* ref_end_gpu;
-    short* query_start_gpu;
-    short* query_end_gpu;
-    short* scores_gpu;
-    unsigned* offset_ref_gpu;
-    unsigned* offset_query_gpu;
+  public:
+    short*    ref_start_gpu    = nullptr;
+    short*    ref_end_gpu      = nullptr;
+    short*    query_start_gpu  = nullptr;
+    short*    query_end_gpu    = nullptr;
+    short*    scores_gpu       = nullptr;
+    unsigned* offset_ref_gpu   = nullptr;
+    unsigned* offset_query_gpu = nullptr;
 
     gpu_alignments(int max_alignments);
     ~gpu_alignments();
