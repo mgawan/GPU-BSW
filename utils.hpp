fb494be5e24b712dd32d30c82c5fb1cf897617e1
Richard Barnes <rbarnes@umn.edu>
2020-07-11 10:36:21 -0600

    Compile code for all architectures and check that kernels launched successfully

--- utils.hpp
+++ utils.hpp
@@ -1,10 +1,14 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 #include "driver.hpp"
+
 #define cudaErrchk(ans)                                                                  \
     {                                                                                    \
         gpuAssert((ans), __FILE__, __LINE__);                                            \
     }
+
+
+
 inline void
 gpuAssert(cudaError_t code, const char* file, int line, bool abort = true)
 {
